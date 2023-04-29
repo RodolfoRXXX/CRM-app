24d71fc7593187d04c7fa3fcc0c8555937c7e458
Rodolfo <rodolforomero.dev@gmail.com>
2023-04-29 17:25:45 -0300

    Trabajando sobre register, problemas sobre repeat password

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 import { AuthService } from 'src/app/services/auth.service';
@@ -7,10 +8,14 @@ import { AuthService } from 'src/app/services/auth.service';
   selector: 'app-register',
   templateUrl: './register.component.html'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
-  hide = true;
+  emailReg = new RegExp("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$");
+  hide_1 = true;
+  hide_2 = true;
+  registerForm!: FormGroup;
   enterprises: Array<any> = [];
+  passwordFirst!: FormControl;
 
   constructor(
     private _auth: AuthService,
@@ -18,7 +23,33 @@ export class RegisterComponent {
     private _router: Router
   ) { 
     this.getEnterprises();
+    this.passwordFirst = new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(10)
+    ]);
    }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.registerForm = new FormGroup({
+        email : new FormControl('', [
+          Validators.required,
+          (control: AbstractControl):ValidationErrors|null => {
+          return !this.emailReg.test(control.value) ? {error_format: {value: control.value}} : null;}
+        ]),
+        password : new FormControl('', [
+          Validators.required,
+          (control: AbstractControl):ValidationErrors|null => {
+          return (control.value !== this.passwordFirst.value) ? {no_equal: {value: control.value}} : null;}
+        ]),
+        remember_me : new FormControl('')
+    }
+    );
+  }
 
   getEnterprises() {
     this._api.getTypeRequest(`enterprise`).subscribe({
@@ -37,4 +68,43 @@ export class RegisterComponent {
     })
   }
 
+  hidePassword_1(ev: any): void {
+    ev.preventDefault();
+    this.hide_1 = !this.hide_1;
+  }
+
+  hidePassword_2(ev: any): void {
+    ev.preventDefault();
+    this.hide_2 = !this.hide_2;
+  }
+
+  getEmailErrorMessage() {
+    if(this.registerForm.controls['email'].hasError('required')) {
+      return 'Tenés que ingresar un valor'}
+    if(this.registerForm.controls['email'].hasError('error_format')) {
+      return 'No es un correo válido'}
+    return ''
+  }
+  getPasswordErrorMessageFirst() {
+    if(this.passwordFirst.hasError('required')) {
+      return 'Tenés que ingresar un valor'}
+    if(this.passwordFirst.hasError('minlength')) {
+      return 'Min. 4 caracteres'}
+    if(this.passwordFirst.hasError('maxlength')) {
+      return 'Max. 10 caracteres'}
+    return ''
+  }
+  getPasswordErrorMessage() {
+    if(this.registerForm.controls['password'].hasError('required')) {
+      return 'Tenés que ingresar un valor'}
+    if(this.registerForm.controls['password'].hasError('no_equal')) {
+      return 'Las contraseñas no coinciden'}
+    return ''
+  }
+
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
+
 }
+
--- src/app/auth/register/register.module.ts
+++ src/app/auth/register/register.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
 import { MaterialModule } from 'src/app/material/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { MaterialModule } from 'src/app/material/material/material.module';
   imports: [
     CommonModule,
     RegisterRoutingModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class RegisterModule { }
