1651c6a0edfac3eddec44eee34ce5bc73dde3946
Rodolfo <rodolfo.singular@gmail.com>
2024-06-04 15:28:53 -0300

    Fix: obtengo los datos de cada producto en particular y los cargo en la aplicación, también modifico un poco el estilo, remarcando los datos que vé el administrador

--- src/app/shared/standalone/dialog/dialog-product-detail/dialog-product-detail.component.ts
+++ src/app/shared/standalone/dialog/dialog-product-detail/dialog-product-detail.component.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { ApiService } from 'src/app/services/api.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Product } from 'src/app/shared/interfaces/product.interface';
+import { environment } from 'src/enviroments/enviroment';
 
 
 @Component({
@@ -18,32 +20,23 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class DialogProductDetailComponent implements OnInit {
 
+  product!: Product;
+  baseURL = environment.SERVER;
+  load: boolean = true;
+
   constructor(
     public dialogRef: MatDialogRef<DialogProductDetailComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private _api: ApiService,
     private _auth: AuthService,
-  ) {
-    //this.setDataUser();
-    console.log(this.data)
-  }
+  ) {}
 
   ngOnInit(): void {
-    this._api.postTypeRequest('profile/get-product-detail', { id_product: this.data.id_product }).subscribe( (value:any) => {
-      console.log(value)
-    })
-  }
-
-  async getDataLocal(): Promise<any> {
-    const data = JSON.parse(this._auth.getDataFromLocalStorage());
-    return data.id_enterprise;
-  }
-
-  setDataUser() {
-    this.getDataLocal()
-        .then( id_enterprise => {
-          
-        })
+    this._api.postTypeRequest('profile/get-product-detail', { id_product: this.data.id_product }).subscribe( (value: any) => {
+      this.product = value.data[0];
+      this.load = false;
+      console.log(this.product)
+    } )
   }
 
   closeDialog() {
