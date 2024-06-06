60c8ff08724663a27f8633b71879067c1502af30
Rodolfo <rodolfo.singular@gmail.com>
2024-06-05 22:54:12 -0300

    Fix: tengo pendiente solucionar que pasa cuando el producto con ese color y medida no tiene stock y quiero cambiar a otro color o medida, el problema es que cuando elijo otro color, el valor de medida queda en un valor que hace q no exista un producto con esa combinación o viceversa, tengo que ver como lo soluciono

--- src/app/shared/standalone/dialog/dialog-product-detail/dialog-product-detail.component.ts
+++ src/app/shared/standalone/dialog/dialog-product-detail/dialog-product-detail.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, Inject, OnInit} from '@angular/core';
+import { Component, Inject, OnInit} from '@angular/core';
 import { MaterialModule } from 'src/app/material/material/material.module';
 import { CommonModule } from '@angular/common';
 import { ApiService } from 'src/app/services/api.service';
@@ -23,49 +23,47 @@ export class DialogProductDetailComponent implements OnInit {
   product!: Product;
   options1!: OptionProduct[];
   options2!: OptionProduct[];
+  data_product: any;
   baseURL = environment.SERVER;
   load: boolean = true;
 
   constructor(
     public dialogRef: MatDialogRef<DialogProductDetailComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private _api: ApiService,
-    private cdr: ChangeDetectorRef
-  ) {}
-
-  ngOnInit(): void {
-
-    this.searchProduct(this.data.id_enterprise, this.data.name, this.data.id_option_1, this.data.id_option_2);
-
+    private _api: ApiService
+  ) {
+    this.data_product = {id_enterprise: this.data.id_enterprise, name: this.data.name, id_option_1: this.data.id_option_1, id_option_2: this.data.id_option_2};
   }
 
-  searchProduct(id_enterprise: number, name: string, option_1: number, option_2: number): void {
+  ngOnInit(): void {
+    this.searchProduct(this.data_product.id_enterprise, this.data_product.name, this.data_product.id_option_1, this.data_product.id_option_2);
+  }
+
+  searchProduct(id_enterprise: number, name: string, id_option_1: number, id_option_2: number): void {
     this.load = true;
-    console.log(id_enterprise, name, option_1, option_2)
 
     //Obtener la información del producto
-    this._api.postTypeRequest('profile/get-product-detail', { id_enterprise: id_enterprise, name: name, id_option_1: option_1, id_option_2: option_2 }).subscribe( (value: any) => {
+    this._api.postTypeRequest('profile/get-product-detail', { id_enterprise: id_enterprise, name: name, id_option_1: id_option_1, id_option_2: id_option_2 }).subscribe( (value: any) => {
       this.product = value.data[0];
       this.load = false;
-      console.log(this.product)
-    } )
+    })
 
     //Obtener el conjunto de los valores de option_1 para ese producto seleccionado
     this._api.postTypeRequest('profile/get-product-detail-option1', { name : name, id_enterprise: id_enterprise}).subscribe( (value: any) => {
+      console.log(value.data)
       this.options1 = value.data;
-    } )
+    })
 
     //Obtener el conjunto de los valores de option_2 para ese producto seleccionado
     this._api.postTypeRequest('profile/get-product-detail-option2', { name : name, id_enterprise: id_enterprise}).subscribe( (value: any) => {
+      console.log(value.data)
       this.options2 = value.data;
-    } )
-
-    this.actualizarVista()
+    })
   }
 
-  actualizarVista(): void {
-    // Realizar cambios en el componente
-    this.cdr.detectChanges(); // Actualizar manualmente la vista
+  rechargeProduct(id_enterprise: number, name: string, id_option_1: number, id_option_2: number) {
+    this.data_product = {id_enterprise: id_enterprise, name: name, id_option_1: id_option_1, id_option_2: id_option_2};
+    this.ngOnInit();
   }
 
   closeDialog() {
