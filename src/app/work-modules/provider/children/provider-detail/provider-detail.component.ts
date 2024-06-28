a508045b3308403e8bbea0648f32fb5c4c0524ff
Rodolfo <rodolfo.singular@gmail.com>
2024-06-27 22:53:10 -0300

    Create: casi finalizo el estilo del componente provider-detail.

--- src/app/work-modules/provider/children/provider-detail/provider-detail.component.ts
+++ src/app/work-modules/provider/children/provider-detail/provider-detail.component.ts
@@ -1,12 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { Router } from '@angular/router';
-import { catchError, combineLatest, of as observableOf } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 import { ConectorsService } from 'src/app/services/conectors.service';
 import { Employee } from 'src/app/shared/interfaces/employee.interface';
+import { Provider } from 'src/app/shared/interfaces/provider.interface';
 
 @Component({
   selector: 'app-provider-detail',
@@ -15,91 +12,80 @@ import { Employee } from 'src/app/shared/interfaces/employee.interface';
 })
 export class ProviderDetailComponent implements OnInit {
 
+  load: boolean = true;
   employee!: Employee;
-  resultsLength!: number;
-  displayedColumns: string[] = ['order', 'date', 'status', 'ammount'];
-  dataSource = new MatTableDataSource<any>();
-  load = true;
-  recharge = false;
-  chips: any = { search: '' };
-
-  isFlat: boolean = true;
-
-  toggleButtonStyle() {
-    this.isFlat = !this.isFlat;
-  }
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+  id_provider!: number;
+  provider!: Provider;
+  card_values: any = { total_art: null, total_inv: null, last_shop: null };
 
   constructor(
+    private route: ActivatedRoute,
     private _api: ApiService,
     private _conector: ConectorsService,
-    private _paginator: MatPaginatorIntl,
     private _router: Router
-  ) {
-    this._paginator.itemsPerPageLabel = "Registros por página";
-    this._paginator.firstPageLabel = "Primera página";
-    this._paginator.lastPageLabel = "última página";
-    this._paginator.nextPageLabel = "Próxima página";
-    this._paginator.previousPageLabel = "Anterior página";
-  }
+  ) {}
 
   ngOnInit(): void {
     this._conector.setUpdateTitle('Detalle del proveedor');
+    this.route.queryParams.subscribe(params => {
+      this.id_provider = parseInt(params['id_provider']);
+      if (this.id_provider) {
+        this.loadProviderData(this.id_provider);
+      } else {
+        this._router.navigate(['init/main/provider/provider-list']);
+      }
+    });
+  }
+
+  loadProviderData(id_provider: number): void {
+    this.getProvider(id_provider);
     this.getDataLocal();
   }
 
+  getProvider(id_provider: number): void {
+    this._api.postTypeRequest('profile/get-provider-id', { id_provider }).subscribe(
+      (value: any) => {
+        if (value.data) {
+          this.provider = value.data[0];
+          this.load = false;
+        }
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
   getDataLocal(): void {
-    this._conector.getEmployee().subscribe((item: Employee) => {
-      this.employee = item;
-      this.loadProvider();
-    });
-  }
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-
-  loadProvider() {
-    this.load = true;
-    combineLatest([
-      this._api.postTypeRequest('profile/get-count-providers', { id_enterprise: this.employee.id_enterprise }),
-      this._api.postTypeRequest('profile/get-providers', { id_enterprise: this.employee.id_enterprise })
-    ]).pipe(
-      catchError(() => {
-        this.recharge = true;
-        return observableOf(null);
-      })
-    ).subscribe(([countResponse, providersResponse]: any) => {
-      this.recharge = false;
-      this.load = false;
-      if (countResponse) {
-        this.resultsLength = countResponse.data[0].total;
+    this._conector.getEmployee().subscribe(
+      (employee: Employee) => {
+        this.employee = employee;
+        this.getDataCard(employee.id_enterprise, this.id_provider);
+      },
+      error => {
+        console.error(error);
       }
-      if (providersResponse) {
-        this.dataSource.data = providersResponse.data;
+    );
+  }
+
+  getDataCard(id_enterprise: number, id_provider: number): void {
+    this._api.postTypeRequest('profile/get-provider-data', { id_enterprise, id_provider }).subscribe(
+      (value: any) => {
+        this.card_values = {
+          total_art: value.data[0].data || 0,
+          total_inv: value.data[1].data || 0,
+          last_shop: value.data[2].data || 0,
+        };
+      },
+      error => {
+        console.error(error);
       }
-    });
+    );
   }
 
-  getDataCard(id_enterprise: number) {
-    const fecha = new Date();
-    fecha.setDate(fecha.getDate() - 60);
-    const year = fecha.getFullYear();
-    const month = fecha.getMonth() + 1;
-    const day = fecha.getDate();
-    const formattedDate = `${year}-${(month < 10 ? '0' : '') + month}-${(day < 10 ? '0' : '') + day}`;
+  editProvider(id_provider: number): void {
+    this._router.navigate(['init/main/provider/provider-edit'], { queryParams: { id_provider } });
   }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-
-  rechargeData() {
-    this.loadProvider();
-  }
-
 }
+
+
