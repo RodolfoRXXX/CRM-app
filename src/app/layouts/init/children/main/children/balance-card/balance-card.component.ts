b6ed77f8f3666f7af60b06eda042a4988da0728f
Rodolfo <rodolfo.singular@gmail.com>
2024-08-05 20:14:28 -0300

    Fix: trabajando en balance.

--- src/app/layouts/init/children/main/children/balance-card/balance-card.component.ts
+++ src/app/layouts/init/children/main/children/balance-card/balance-card.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-balance-card',
   templateUrl: './balance-card.component.html',
   styleUrls: ['./balance-card.component.scss']
 })
-export class BalanceCardComponent {
+export class BalanceCardComponent implements OnInit {
 
+  basicData: any;
+  basicOptions: any;
+
+  ngOnInit() {
+    this.initializeChartOptions();
+    this.initializeChartData();
+  }
+
+  initializeChartData() {
+    this.basicData = {
+      labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+      datasets: [
+        {
+          label: 'Sales',
+          data: [540, 325, 702, 620],
+          backgroundColor: [
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)'
+          ],
+          borderColor: [
+            'rgb(255, 159, 64)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)'
+          ],
+          borderWidth: 1
+        }
+      ]
+    };
+  }
+
+  initializeChartOptions() {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+    const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+    this.basicOptions = {
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor
+          }
+        }
+      },
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: {
+            color: textColorSecondary
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false
+          }
+        },
+        x: {
+          ticks: {
+            color: textColorSecondary
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false
+          }
+        }
+      }
+    };
+  }
 }
+
