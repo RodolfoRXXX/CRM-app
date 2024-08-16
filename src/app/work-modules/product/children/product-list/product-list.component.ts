9d261370f8c8d3d97371b6005d710003b63219c6
Rodolfo <rodolfo.singular@gmail.com>
2024-08-16 15:48:09 -0300

    Fix: corrijo el problema con los filtros faltantes.

--- src/app/work-modules/product/children/product-list/product-list.component.ts
+++ src/app/work-modules/product/children/product-list/product-list.component.ts
@@ -1,10 +1,11 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
+import { MatSelect } from '@angular/material/select';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
-import { catchError, map, merge, startWith, switchMap, of as observableOf } from 'rxjs';
+import { forkJoin } from 'rxjs';
 import { ApiService } from 'src/app/services/api.service';
 import { ConectorsService } from 'src/app/services/conectors.service';
 import { calculateDateLimit } from 'src/app/shared/functions/date.function';
@@ -17,7 +18,12 @@ import { environment, permissions } from 'src/enviroments/enviroment';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent implements OnInit, AfterViewInit {
+export class ProductListComponent implements OnInit {
+
+  @ViewChild('search') search!: ElementRef;
+  @ViewChild('category') category!: ElementRef;
+  @ViewChild('is_stock') is_stock!: ElementRef;
+  @ViewChild('state') state!: ElementRef;
 
   employee!: Employee;
   categories!: any[];
@@ -29,10 +35,11 @@ export class ProductListComponent implements OnInit, AfterViewInit {
   load_card2!: boolean;
   load_card3!: boolean;
   load_card4!: boolean;
-  load!: boolean;
-  recharge!: boolean;
-  empty_products!: boolean;
-  chips: any = {category: '', stock: '', state: '', search: ''};
+  load: boolean = true;
+  recharge: boolean = false;
+  empty_products: boolean = false;
+  chips: any = {};
+  chips_arr: any = {};
   card_values: any = {products_with_stock: null, value_stock: null, products_without_stock: null, immo_stock: null};
   permissions: string[] = [];
   add_product_admin = permissions.EDIT_PRODUCT_CONTROL;
@@ -49,10 +56,7 @@ export class ProductListComponent implements OnInit, AfterViewInit {
     private _dialog: MatDialog,
     private _router: Router
   ) {
-    this.load = true;
-    this.recharge = false;
-    this.empty_products = false;
-    this.getProducts();
+    //this.getProducts();
     this._paginator.itemsPerPageLabel = "Registros por página";
     this._paginator.firstPageLabel = "Primera página";
     this._paginator.lastPageLabel = "última página";
@@ -63,38 +67,20 @@ export class ProductListComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     //Modifica el título de la vista principal
     this._conector.setUpdateTitle('Lista de productos')
+    this.getDataLocal()
   }
 
-  getDataLocal(): number {
+  //Getters
+  getDataLocal() {
     this._conector.getEmployee().subscribe( (item:Employee) => {
       this.employee = item
-      //la lista de permisos se almacena como un string y luego se lo separa en un array
-      //aunque el string de la DB esté vacío, el split devuelve un array con al menos un valor,
-      //que es el valor vacío, por eso la desigualdad es mayor a 1
       this.permissions = item.list_of_permissions.split(',')
+      this.loadData()
+      this.getTags(this.employee.id_enterprise)
+      this.getCategories(this.employee.id_enterprise)
+      this.getDataCard(this.employee.id_enterprise)
     })
-    return this.employee.id_enterprise;
   }
-
-  getProducts(): void {
-    merge()
-      .pipe(
-        startWith({}),
-        map(() => this.getDataLocal()),
-        switchMap((id_enterprise) => {
-          this.getDataCard(id_enterprise);
-          this.getCategories(id_enterprise);
-          this.getTags(id_enterprise);
-          return this._api.postTypeRequest('profile/get-count-products', { id_enterprise: id_enterprise })
-                        .pipe(catchError(async () => {observableOf(null); this.recharge = true;}));
-        }),
-        map(data => {
-          return data;
-        })
-      )
-    .subscribe((data: any) => this.resultsLength = data.data[0].total);
-  }
-
   getCategories(id_enterprise: number): void {
     this._api.postTypeRequest('profile/get-categories', { id_enterprise: id_enterprise }).subscribe( (value:any) => {
       this.categories = value.data
@@ -103,7 +89,6 @@ export class ProductListComponent implements OnInit, AfterViewInit {
       });
     })
   }
-
   getTags(id_enterprise: number): void {
     this._api.postTypeRequest('profile/get-filters-obj', { id_enterprise: id_enterprise }).subscribe( (value:any) => {
       if(value.data) {
@@ -114,26 +99,6 @@ export class ProductListComponent implements OnInit, AfterViewInit {
       }
     })
   }
-
-  setFilter(id_filter: number) {
-    this.load = true;
-    this.empty_products = false;
-    console.log(id_filter)
-    this._api.postTypeRequest('profile/get-product-filter-id', { id_enterprise: this.employee.id_enterprise, id_filter: id_filter }).subscribe( (value:any) => {
-      this.load = false;
-      console.log(value.data)
-        if (value.data) {
-          value.data.forEach((item: any) => {
-            item.category_color = JSON.parse(item.category_color)
-          });
-        this.dataSource.data = value.data;
-        } else {
-          //Sin resultados
-          this.empty_products = true;
-        }
-    })
-  }
-
   getDataCard(id_enterprise: number) {
     let date_limit = calculateDateLimit(60);
     this._api.postTypeRequest('profile/get-products-data', { id_enterprise: id_enterprise, date_limit: date_limit }).subscribe( (value:any) => {
@@ -144,63 +109,86 @@ export class ProductListComponent implements OnInit, AfterViewInit {
     })
   }
 
-  applyFilter() {
-    let i = 0;
-    for (let key in this.chips){
-      this.chips[key]?this.dataSource.filter = this.chips[key]:i++;
+  //Función que trae los valores desde la DB
+  loadData(): void {
+    if (this.employee.id_enterprise) {
+      this.empty_products = false;
+      this.load = true;
+      forkJoin({
+        count: this._api.postTypeRequest('profile/get-count-products', 
+          { id_enterprise: this.employee.id_enterprise, search: this.chips_arr.search, category: this.chips_arr.category, is_stock: this.chips_arr.is_stock, state: this.chips_arr.state, filters: this.chips_arr.filters }),
+        products: this._api.postTypeRequest('profile/get-products', 
+          { id_enterprise: this.employee.id_enterprise, search: this.chips_arr.search, category: this.chips_arr.category, is_stock: this.chips_arr.is_stock, state: this.chips_arr.state, filters: this.chips_arr.filters })
+      }).subscribe({
+        next: (results: any) => {
+          if(results.count.data[0].total > 0) {
+            this.resultsLength = results.count.data.total
+            results.products.data.forEach((item: any) => {
+              item.category_color = JSON.parse(item.category_color)
+            });
+            this.dataSource.data = results.products.data
+            this.dataSource.paginator = this.paginator;
+          } else {
+            this.empty_products = true;
+          }
+          this.load = false;
+        },
+        error: () => {
+          this.load = false;
+        }
+      });
     }
-    (i == 4)?this.dataSource.filter = '':'';
   }
-  add(value: Event, key: string) {
-    let chip_str
 
-    //Verifica que el value que entra sea un string o un evento
-    if(typeof value != 'string') {
-      //Si es un evento entonces lo lee como tal y recupera el string 
-      chip_str = ((value.target as HTMLInputElement).value);
-    } else {
-      //Si es un string entonces lo pasa como tal
-      chip_str = value
+  //Funciones que manejan la visulización de chips y los filtros
+  add(value: any, key: string) {
+    switch (key) {
+      case 'search':
+        this.chips.search = ((value.target as HTMLInputElement).value).trim().toLowerCase()
+        this.chips_arr.search = ((value.target as HTMLInputElement).value).trim().toLowerCase()
+        break;
+      case 'category':
+        this.chips.category = value.name
+        this.chips_arr.category = value.id
+        break;
+      case 'is_stock':
+        this.chips.is_stock = value
+        this.chips_arr.is_stock = value
+        break;
+      case 'state':
+        this.chips.state = value
+        this.chips_arr.state = value
+        break;
+      case 'filters':
+        this.chips_arr.filters = `${value.id}`;
+        this.chips.filters = value.name
+        break;
     }
-    //Primero verifica que la variable interna asignada tenga un valor
-    if(chip_str.length > 0) {
-      this.chips[key] = chip_str.trim().toLowerCase();
-      this.applyFilter();
-    }
+    this.loadData()
   }
   delete(key: any) {
-    //Recibe la clave que debe borrar tomada desde el chip que representa el filtro aplicado y revaloriza el objeto chips a cadena vacía
-    this.chips[key] = ''
-    this.applyFilter();
+    delete this.chips[key];
+    delete this.chips_arr[key];
+
+    if (key !== 'filters' && this[key as keyof this]) {
+        const element = this[key as keyof this];
+
+        // Verifica si el elemento es un MatSelect
+        if (element instanceof MatSelect) {
+            element.value = null;
+        }
+        // Verifica si el elemento tiene la propiedad `nativeElement`
+        else if ((element as any).nativeElement) {
+            (element as any).nativeElement.value = '';
+        }
+    }
+
+    this.loadData();
   }
 
   ngAfterViewInit() {
-    merge(this.paginator.page)
-      .pipe(
-        startWith({}),
-        map(() => this.getDataLocal()),
-        switchMap((id_enterprise) => {
-          this.recharge = false;
-          this.load = true;
-          return this._api.postTypeRequest('profile/get-products', { id_enterprise: id_enterprise, page: this.paginator.pageIndex, size: 10 })
-                        .pipe(catchError(async () => {observableOf(null); this.recharge = true;}));
-        }),
-        map(data => {
-          return data;
-        })
-      )
-      .subscribe((data: any) => {
-        this.load = false;
-        if (data.data) {
-          data.data.forEach((item: any) => {
-            item.category_color = JSON.parse(item.category_color)
-          });
-          this.dataSource.data = data.data;
-        } else {
-          this.empty_products = true;
-        }
-      });
-      this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
   //Función que toma la fila clickeada del table eligiendo esa opción
@@ -210,16 +198,19 @@ export class ProductListComponent implements OnInit, AfterViewInit {
     }
   }
 
+  //Redirecciona para editar el producto
   editProduct(id_product: number) {
     this._router.navigate(['init/main/product/add-product'], { queryParams: { id_product: id_product } });
   }
 
+  //Abre el detalle del producto
   openDialogDetail(id_product: number): void {
       const dialogRef = this._dialog.open(DialogProductDetailComponent, { data: { id_product: id_product } });
   }
 
+  //Recarga la información
   rechargeData() {
-    this.ngAfterViewInit();
+    this.loadData();
   }
 
 }
