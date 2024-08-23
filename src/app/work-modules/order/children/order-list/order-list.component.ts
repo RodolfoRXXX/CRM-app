88a122f604a8886fb8b42f8fb4e2161adc7b1edc
Rodolfo <rodolfo.singular@gmail.com>
2024-08-23 16:51:02 -0300

    Fix: termino de arreglar los errores del componente orders con el filtrado

--- src/app/work-modules/order/children/order-list/order-list.component.ts
+++ src/app/work-modules/order/children/order-list/order-list.component.ts
@@ -1,9 +1,12 @@
-import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { Component, ViewChild, OnInit, AfterViewInit, ElementRef } from '@angular/core';
+import { MatDatepickerInput } from '@angular/material/datepicker';
 import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
+import { MatSelect } from '@angular/material/select';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
-import { merge, startWith, map, switchMap, catchError, of as observableOf } from 'rxjs';
+import { merge, startWith, map, switchMap, catchError, of as observableOf, forkJoin } from 'rxjs';
 import { ApiService } from 'src/app/services/api.service';
 import { ConectorsService } from 'src/app/services/conectors.service';
 import { calculateDateLimit } from 'src/app/shared/functions/date.function';
@@ -17,15 +20,22 @@ import { environment, permissions } from 'src/enviroments/enviroment';
 })
 export class OrderListComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('dateTime') dateTime!: ElementRef;
+  @ViewChild('sellerF') sellerF!: ElementRef;
+  @ViewChild('state') state!: ElementRef;
+
   employee!: Employee;
   sellers!: any;
   displayedColumns: string[] = ['id', 'date', 'customer', 'seller', 'status'];
   dataSource = new MatTableDataSource();
+  empty_orders = false;
   resultsLength!: number;
   loadCards: boolean = true;
   load = true;
   recharge = false;
-  chips: any = { search: '', status: '', seller: '' };
+  chips: any = {};
+  chips_arr: any = {};
+  permissions: string[] = [];
   //d1: pendientes, d2: entregados, d3: devoluciones, d4: cancelaciones
   card_values: any = { d1: null, d2: null, d4: null, d5: null };
   uriImg = environment.SERVER;
@@ -42,11 +52,11 @@ export class OrderListComponent implements OnInit, AfterViewInit {
     private _router: Router
   ) {
     this.initPaginatorLabels();
-    this.getOrders();
   }
 
   ngOnInit(): void {
     this._conector.setUpdateTitle('Lista de pedidos');
+    this.getDataLocal();
   }
 
   private initPaginatorLabels(): void {
@@ -57,34 +67,28 @@ export class OrderListComponent implements OnInit, AfterViewInit {
     this._paginatorIntl.previousPageLabel = "Anterior página";
   }
 
-  private getDataLocal(): number {
-    this._conector.getEmployee().subscribe((item: Employee) => {
-      this.employee = item;
+  //Getters
+  private getDataLocal(): void {
+    this._conector.getEmployee().subscribe( (item:Employee) => {
+      this.employee = item
+      this.permissions = item.list_of_permissions.split(',')
       if(!item.list_of_permissions.includes(this.edit_enterprise_control)) {
         this.seller = item.id;
       } else {
         this.seller = null;
         this.getSellers(item.id_enterprise)
       }
+      this.loadData()
+      this.getDataCard(this.employee.id_enterprise)
+    })
+  }
+  private getSellers(id_enterprise: number) {
+    this._api.postTypeRequest('profile/get-enterprise-users', { id_enterprise }).subscribe((value: any) => {
+      if(value) {
+        this.sellers = value.data;
+      }
     });
-    return this.employee.id_enterprise;
   }
-
-  private getOrders(): void {
-    merge()
-      .pipe(
-        startWith({}),
-        map(() => this.getDataLocal()),
-        switchMap(id_enterprise => {
-          this.getDataCard(id_enterprise);
-          return this._api.postTypeRequest('profile/get-count-orders', { id_enterprise, seller: this.seller })
-            .pipe(catchError(() => observableOf(null)));
-        }),
-        map(data => data)
-      )
-      .subscribe((data: any) => this.resultsLength = data.data[0].total);
-  }
-
   private getDataCard(id_enterprise: number): void {
     const date_limit = calculateDateLimit(365);
     this._api.postTypeRequest('profile/get-orders-data', { id_enterprise, date_limit, seller: this.seller }).subscribe((value: any) => {
@@ -117,61 +121,91 @@ export class OrderListComponent implements OnInit, AfterViewInit {
     });
   }
 
-  private getSellers(id_enterprise: number) {
-    this._api.postTypeRequest('profile/get-enterprise-users', { id_enterprise }).subscribe((value: any) => {
-      if(value) {
-        this.sellers = value.data;
+//Función que trae los valores desde la DB
+loadData(): void {
+  if (this.employee.id_enterprise) {
+    this.empty_orders = false;
+    this.load = true;
+    forkJoin({
+      count: this._api.postTypeRequest('profile/get-count-orders', 
+        { id_enterprise: this.employee.id_enterprise, dateTime: this.chips_arr.dateTime, sellerF: this.chips_arr.sellerF, state: this.chips_arr.state, seller: this.seller }),
+      orders: this._api.postTypeRequest('profile/get-orders', 
+        { id_enterprise: this.employee.id_enterprise, dateTime: this.chips_arr.dateTime, sellerF: this.chips_arr.sellerF, state: this.chips_arr.state, seller: this.seller })
+    }).subscribe({
+      next: (results: any) => {
+        if(results.count.data[0].total > 0) {
+          results.orders.data.forEach((element: any) => {
+            if(element.status == 1) {
+              element.status = 'Abierto'
+            } else {
+              element.status = 'Cerrado'
+            }
+          });
+          this.resultsLength = results.count.data.total
+          this.dataSource.data = results.orders.data
+          this.dataSource.paginator = this.paginator;
+        } else {
+          this.empty_orders = true;
+        }
+        this.load = false;
+      },
+      error: () => {
+        this.load = false;
       }
     });
   }
+}
 
   getEmployeeDisplayName(item: any): string {
     return item.name_employee || item.email.split("@")[0];
   }
 
-  applyFilter(): void {
-    const filters = Object.values(this.chips).filter(value => value).join(' ');
-    this.dataSource.filter = filters || '';
-  }
-  add(value: string | Event, key: string): void {
-    const chipValue = typeof value === 'string' ? value : (value.target as HTMLInputElement).value;
-    if (chipValue.trim()) {
-      this.chips[key] = chipValue.trim().toLowerCase();
-      this.applyFilter();
+  //Funciones que manejan la visulización de chips y los filtros
+  add(value: any, key: string) {
+    switch (key) {
+      case 'dateTime':
+        this.chips.dateTime = formatDate(value, 'dd-MM-yyyy', 'en');
+        this.chips_arr.dateTime = formatDate(value, 'yyyy-MM-dd', 'en');;
+        break;
+      case 'sellerF':
+        this.chips.sellerF = value.name
+        this.chips_arr.sellerF = value.id
+        break;
+      case 'state':
+        this.chips.state = value.name
+        this.chips_arr.state = value.id
+        break;
     }
+    this.loadData()
   }
-  delete(key: any): void {
-    this.chips[key] = '';
-    this.applyFilter();
-  }
+  delete(key: any) {
+    console.log(key)
+    delete this.chips[key];
+    delete this.chips_arr[key];
 
-  ngAfterViewInit(): void {
-    merge(this.paginator.page)
-      .pipe(
-        startWith({}),
-        map(() => this.getDataLocal()),
-        switchMap(id_enterprise => {
-          this.recharge = false;
-          this.load = true;
-          return this._api.postTypeRequest('profile/get-orders', { id_enterprise, page: this.paginator.pageIndex, size: 10, seller: this.seller })
-            .pipe(catchError(() => observableOf(null)));
-        }),
-        map(data => data)
-      )
-      .subscribe((data: any) => {
-        this.load = false;
-        if (data.data) {
-          data.data.forEach((element: any) => {
-            if(element.status == 1) {
-              element.status = 'Abierto';
-            } else if(element.status == 0) {
-              element.status = 'Cerrado';
-            }
-          });
-          this.dataSource.data = data.data;
-        } 
-      });
+    if (this[key as keyof this]) {
+        const element = this[key as keyof this];
 
+        // Verifica si el elemento es un MatSelect
+        if (element instanceof MatSelect) {
+            element.value = null;
+        }
+        // Verifica si el elemento es un MatDatepickerInput
+        else if (element instanceof MatDatepickerInput) {
+            element.value = null;
+        }
+        // Verifica si el elemento tiene la propiedad `nativeElement`
+        else if ((element as any).nativeElement) {
+            (element as any).nativeElement.value = '';
+        }
+    }
+
+    this.loadData();
+}
+
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
 
@@ -182,8 +216,10 @@ export class OrderListComponent implements OnInit, AfterViewInit {
     }
   }
 
-  rechargeData(): void {
-    this.ngAfterViewInit();
+  //Recarga la información
+  rechargeData() {
+    this.loadData();
   }
+
 }
 
