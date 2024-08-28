c8aed902f177e03052cb9b8312de7b6836ecdf3b
Rodolfo <rodolfo.singular@gmail.com>
2024-08-28 09:33:01 -0300

    Fix: elimino directivas y archivos sin uso

--- src/app/layouts/init/init.module.ts
+++ src/app/layouts/init/init.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { InitRoutingModule } from './init-routing.module';
 import { InitComponent } from './init.component';
-import { UpdateDirective } from 'src/app/shared/directives/update/update.directive';
 import { MaterialModule } from 'src/app/material/material/material.module';
 import { HeaderLoginComponent } from './header/header-login/header-login.component';
 import { HeaderRechargeComponent } from './header/header-recharge/header-recharge.component';
@@ -12,7 +11,6 @@ import { HeaderRechargeComponent } from './header/header-recharge/header-recharg
 @NgModule({
   declarations: [
     InitComponent,
-    UpdateDirective,
     HeaderLoginComponent,
     HeaderRechargeComponent
   ],
